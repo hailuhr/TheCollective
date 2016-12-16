d44c4e07a02ccff4e9676b91b9ddcf7ff7514dd9
Flatiron School <github email address>
2016-12-16 12:18:56 -0500

    added javascript_include_tag application in bootstrap application.html

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,9 +1,22 @@
 $(function() {
+  // alert('ready');
+  $("#view_locations").on("click", locationsList)
 
-  locationsList()
+  $(".test").on("click", test)
 })
 
 
-function locationsList() {
-  
+function locationsList(e) {
+  e.preventDefault()
+  $.ajax({
+    method: "GET",
+    url: "/"
+  })
+}
+
+
+function test() {
+  alert("lakjdfs")
+  $(".home").prepend("adlkjfslf")
+
 }
