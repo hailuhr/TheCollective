122572043e6c89d49067ec8f42b602d0e1cce4c5
Flatiron School <github email address>
2017-02-09 23:37:59 -0500

    removed debuggers from home/class.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,26 +1,11 @@
 $(document).on('turbolinks:load', function() {
 
-  //  $.ajax to '/locations'
-  //  .then( response => {
-  //     // let markerData = response.locations.map( loc => {lat: loc.lat, lng: loc.lng})
-  //     debugger;
-  //     handler.buildMap({ internal: {id: 'map'}}, function(){
-  //       // var markers = handler.addMarkers(markerData);
-  //       var markers = handler.addMarkers(markers);
-  //     handler.bounds.extendWith(markers);
-  //     handler.fitMapToBounds();
-  // });
-  // })
-
-
       $.ajax({
         method: "GET",
         url: "/locations/geocode",
         success: function(data) {
-          // debugger;
           var handler = Gmaps.build('Google');
           handler.buildMap({ internal: {id: 'map'}}, function(){
-            // var markers = handler.addMarkers(markerData);
           var markers = handler.addMarkers(data);
           handler.bounds.extendWith(markers);
           handler.fitMapToBounds();
